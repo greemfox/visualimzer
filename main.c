245cd230b2d6a5bf8b4729154913b9b0c98f4398
greemfox <pavelkurchanov.lpk@gmail.com>
2026-02-01 12:12:04 +0700

    parse the header

--- main.c
+++ main.c
@@ -2,21 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fpos_log(FILE *f)
-{
-	printf("current file position: %lu\n", ftell(f));
-}
-
 bool parse_wav(char *path)
 {
 	FILE *f = fopen(path, "rb");
-	u32 header_size;
+	u32 file_size;
 	u32 format_size;
 	u16 format_tag;
 	u16 channels;
 	u32 samples_per_sec;
 	u32 avg_bytes_per_sec;
 	u16 block_align;
+	u16 bits_per_sample;
+	u32 info_size;
 
 	if (fgetc(f) != 'R' || fgetc(f) != 'I' || fgetc(f) != 'F' ||
 	    fgetc(f) != 'F') {
@@ -24,7 +21,8 @@ bool parse_wav(char *path)
 		return false;
 	}
 
-	fread(&header_size, 4, 1, f);
+	fread(&file_size, 4, 1, f);
+	printf("File size: %i bytes\n", file_size);
 
 	if (fgetc(f) != 'W' || fgetc(f) != 'A' || fgetc(f) != 'V' ||
 	    fgetc(f) != 'E') {
@@ -34,24 +32,49 @@ bool parse_wav(char *path)
 
 	if (fgetc(f) != 'f' || fgetc(f) != 'm' || fgetc(f) != 't' ||
 	    fgetc(f) != ' ') {
-		fprintf(stderr, "ERROR: %s is missing the fmt chunk\n", path);
+		fprintf(stderr, "ERROR: couldn't find the fmt chunk in %s\n",
+			path);
 		return false;
 	}
 
 	fread(&format_size, 4, 1, f);
-	fread(&format_tag, 2, 1, f);
-	fread(&channels, 2, 1, f);
-	fread(&samples_per_sec, 4, 1, f);
-	fread(&avg_bytes_per_sec, 4, 1, f);
-	fread(&block_align, 2, 1, f);
-
-	printf("Header size: %i bytes\n", header_size);
 	printf("Format size: %i bytes\n", format_size);
+
+	fread(&format_tag, 2, 1, f);
 	printf("Format tag: %04x\n", format_tag);
+	if (format_tag != 1) {
+		fprintf(stderr, "SORRY! IBM formats are not supported\n");
+		return false;
+	}
+
+	fread(&channels, 2, 1, f);
 	printf("Channels: %i\n", channels);
+
+	fread(&samples_per_sec, 4, 1, f);
 	printf("Samples per sec: %i Hz\n", samples_per_sec);
+
+	fread(&avg_bytes_per_sec, 4, 1, f);
 	printf("Avg bytes per sec: %i bytes\n", avg_bytes_per_sec);
-	printf("Block align: %i bytes\n", block_align);
+
+	fread(&block_align, 2, 1, f);
+	printf("Block alignment: %i bytes\n", block_align);
+
+	fread(&bits_per_sample, 2, 1, f);
+	printf("Bits per sample: %i\n", bits_per_sample);
+
+	fseek(f, 4, SEEK_CUR);
+	fread(&info_size, 4, 1, f);
+	printf("Info size: %i bytes\n", info_size);
+
+	fseek(f, info_size, SEEK_CUR);
+	printf("Skipping info...\n");
+
+	if (fgetc(f) != 'd' || fgetc(f) != 'a' || fgetc(f) != 't' ||
+	    fgetc(f) != 'a') {
+		fprintf(stderr, "ERROR: couldn't find the data chunk in %s\n",
+			path);
+		return false;
+	}
 
 	return true;
 }
